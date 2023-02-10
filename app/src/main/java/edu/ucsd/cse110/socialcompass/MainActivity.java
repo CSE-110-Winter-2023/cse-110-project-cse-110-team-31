8b7e34e1c6af3c78e4a687db8584b3017848b347
kenva <ken.vandeventer@outlook.com>
2023-02-10 15:42:12 -0800

    Revert "added add locations activity and stored data. (no unit tests)"
    This reverts commit a58b28763436be3fba1f2c96b2cc2429a474e1a6.

--- app/src/main/java/edu/ucsd/cse110/socialcompass/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/socialcompass/MainActivity.java
@@ -2,20 +2,12 @@ package edu.ucsd.cse110.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-
-        Intent add_locations_activity = new Intent(this, AddLocationsActivity.class);
-        startActivity(add_locations_activity);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
