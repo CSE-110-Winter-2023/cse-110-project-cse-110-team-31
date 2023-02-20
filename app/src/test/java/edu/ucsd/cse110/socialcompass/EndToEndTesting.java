9b1070a68301a92545c6144348fcf75f624db2f5
Rana Lulla <ranalulla@Ranas-MacBook-Pro.local>
2023-02-19 21:43:18 -0800

    refactored test names and added new ones

---
+++ app/src/test/java/edu/ucsd/cse110/socialcompass/EndToEndTesting.java
@@ -0,0 +1,79 @@
+/**
+ * Testing for the entire milestone
+ * This was originally testing for story 4, but since that was our last story
+ * it ended up testing the entire MS
+ */
+
+package edu.ucsd.cse110.socialcompass;
+
+import android.util.Pair;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.MutableLiveData;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class EndToEndTesting {
+    @Rule
+    public GrantPermissionRule finePermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+    @Rule
+    public GrantPermissionRule coarsePermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
+    @Test
+    public void testIntegration() {
+        var mockOrientation = new MutableLiveData<Float>();
+        var mockLocation = new MutableLiveData<Pair<Double, Double>>();
+
+        var scenarioAddLocations = ActivityScenario.launch(AddLocationsActivity.class);
+        scenarioAddLocations.moveToState(Lifecycle.State.CREATED);
+        scenarioAddLocations.moveToState(Lifecycle.State.STARTED);
+
+        scenarioAddLocations.onActivity(activity -> {
+            TextView friend_long_view = (TextView) activity.findViewById(R.id.friend_long_view);
+            TextView friend_lat_view = (TextView) activity.findViewById(R.id.friend_lat_view);
+            TextView house_long_view = (TextView) activity.findViewById(R.id.home_long_view);
+            TextView house_lat_view = (TextView) activity.findViewById(R.id.home_lat_view);
+            Button submit_button = (Button) activity.findViewById(R.id.submit_button);
+            friend_long_view.setText("10");
+            friend_lat_view.setText("5");
+            house_long_view.setText("5");
+            house_lat_view.setText("10");
+            submit_button.performClick();
+        });
+
+        var scenario = ActivityScenario.launch(CompassActivity.class);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            activity.locationService.setMockOrientationSource(mockLocation);
+            activity.updateLocation();
+            activity.orientationService.setMockOrientationData(mockOrientation);
+            activity.updateOrientation();
+
+            mockOrientation.setValue(0f);
+            mockLocation.setValue(new Pair<>(5d, 5d));
+
+            ImageView friend = activity.findViewById(R.id.friend);
+            ConstraintLayout.LayoutParams friendLayoutParams = (ConstraintLayout.LayoutParams) friend.getLayoutParams();
+            ImageView house = activity.findViewById(R.id.house);
+            ConstraintLayout.LayoutParams houseLayoutParams = (ConstraintLayout.LayoutParams) house.getLayoutParams();
+
+            assert houseLayoutParams.circleAngle == 0 && friendLayoutParams.circleAngle == 90;
+
+            mockOrientation.setValue((float)Math.PI/2); // rotate 90 degrees
+
+            assert houseLayoutParams.circleAngle == 90 && friendLayoutParams.circleAngle == 180;
+        });
+    }
+}
--- app/src/test/java/edu/ucsd/cse110/socialcompass/Story2IntegrationTest.java
+++ app/src/test/java/edu/ucsd/cse110/socialcompass/Story2IntegrationTest.java
@@ -1,59 +1,143 @@
 package edu.ucsd.cse110.socialcompass;
 
-import android.util.Pair;
-import android.widget.ImageView;
+import static org.junit.Assert.assertEquals;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.MutableLiveData;
 import androidx.test.core.app.ActivityScenario;
-import androidx.test.rule.GrantPermissionRule;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class Story2IntegrationTest {
-    @Rule
-    public GrantPermissionRule finePermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
-
-    @Rule
-    public GrantPermissionRule coarsePermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_COARSE_LOCATION);
 
     @Test
-    public void testIntegration() {
-        var mockOrientation = new MutableLiveData<Float>();
-        var mockLocation = new MutableLiveData<Pair<Double, Double>>();
-
-        var scenario = ActivityScenario.launch(CompassActivity.class);
+    public void testSubmittedSingularValuePersists() {
+        var scenario = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
 
         scenario.onActivity(activity -> {
-            activity.locationService.setMockOrientationSource(mockLocation);
-            activity.updateLocation();
-            activity.orientationService.setMockOrientationData(mockOrientation);
-            activity.updateOrientation();
-
-            activity.friendLat = 5;
-            activity.friendLon = 10; // straight right
-            activity.houseLat = 10;
-            activity.houseLon = 5; // straight up
-
-            mockOrientation.setValue(0f);
-            mockLocation.setValue(new Pair<>(5d, 5d));
-
-            ImageView friend = activity.findViewById(R.id.friend);
-            ConstraintLayout.LayoutParams friendLayoutParams = (ConstraintLayout.LayoutParams) friend.getLayoutParams();
-            ImageView house = activity.findViewById(R.id.house);
-            ConstraintLayout.LayoutParams houseLayoutParams = (ConstraintLayout.LayoutParams) house.getLayoutParams();
-
-            assert houseLayoutParams.circleAngle == 0 && friendLayoutParams.circleAngle == 90;
-
-            mockOrientation.setValue((float)Math.PI/2); // rotate 90 degrees
-
-            assert houseLayoutParams.circleAngle == 90 && friendLayoutParams.circleAngle == 180;
+            TextView my_long_view = (TextView) activity.findViewById(R.id.family_long_view);
+            Button submit_button = (Button) activity.findViewById(R.id.submit_button);
+            my_long_view.setText("1");
+            submit_button.performClick();
         });
+
+        var scenario2 = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario2.moveToState(Lifecycle.State.CREATED);
+        scenario2.moveToState(Lifecycle.State.STARTED);
+
+        scenario2.onActivity(activity2 -> {
+            TextView my_long_view = (TextView) activity2.findViewById(R.id.family_long_view);
+            assertEquals("1.0", my_long_view.getText().toString());
+        });
+    }
+
+    @Test
+    public void testSubmittingMultipleValuesPersists(){
+        var scenario = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            TextView my_long_view = (TextView) activity.findViewById(R.id.family_long_view);
+            TextView my_long2_view = (TextView) activity.findViewById(R.id.friend_long_view);
+            TextView my_lat3_view = (TextView) activity.findViewById(R.id.family_lat_view);
+            Button submit_button = (Button) activity.findViewById(R.id.submit_button);
+
+            my_long_view.setText("1");
+            my_long2_view.setText("2");
+            my_lat3_view.setText("3");
+            submit_button.performClick();
+
+            var scenario2 = ActivityScenario.launch(AddLocationsActivity.class);
+            scenario2.moveToState(Lifecycle.State.CREATED);
+            scenario2.moveToState(Lifecycle.State.STARTED);
+
+            scenario2.onActivity(activity2 -> {
+                TextView my_long_view_2 = (TextView) activity2.findViewById(R.id.family_long_view);
+                TextView my_long2_view_2 = (TextView) activity2.findViewById(R.id.friend_long_view);
+                TextView my_lat3_view_2 = (TextView) activity2.findViewById(R.id.family_lat_view);
+                assertEquals("1.0", my_long_view_2.getText().toString());
+                assertEquals("2.0", my_long2_view_2.getText().toString());
+                assertEquals("3.0", my_lat3_view_2.getText().toString());
+            });
+
+        });
+    }
+
+    @Test
+    public void testSubmittingNoFullCoords() {
+        var scenario = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            TextView my_long_view = (TextView) activity.findViewById(R.id.family_long_view);
+            Button submit_button = (Button) activity.findViewById(R.id.submit_button);
+            my_long_view.setText("1");
+            submit_button.performClick();
+        });
+
+        assertEquals(scenario.getState(), Lifecycle.State.STARTED);
+    }
+
+    @Test
+    public void testSubmittingFullCoord() {
+        var scenario = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            TextView my_long_view = (TextView) activity.findViewById(R.id.home_long_view);
+            TextView my_lat_view = (TextView) activity.findViewById(R.id.home_lat_view);
+            Button submit_button = (Button) activity.findViewById(R.id.submit_button);
+            my_long_view.setText("1");
+            my_lat_view.setText("2");
+            submit_button.performClick();
+        });
+
+        scenario = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            TextView my_long_view = (TextView) activity.findViewById(R.id.home_long_view);
+            TextView my_lat_view = (TextView) activity.findViewById(R.id.home_lat_view);
+            assertEquals("1.0", my_long_view.getText().toString());
+            assertEquals("2.0", my_lat_view.getText().toString());
+        });
+
+
+    }
+
+    @Test
+    public void testNotSubmittingNotPersists(){
+        var scenario = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            TextView my_long_view = (TextView) activity.findViewById(R.id.family_long_view);
+            Button submit_button = (Button) activity.findViewById(R.id.submit_button);
+            my_long_view.setText("1");
+            scenario.moveToState(Lifecycle.State.DESTROYED);
+        });
+
+        var scenario2 = ActivityScenario.launch(AddLocationsActivity.class);
+        scenario2.moveToState(Lifecycle.State.CREATED);
+        scenario2.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            TextView my_long_view = (TextView) activity.findViewById(R.id.family_long_view);
+            assertEquals("", my_long_view.getText().toString());
+        });
+
+
     }
 }
