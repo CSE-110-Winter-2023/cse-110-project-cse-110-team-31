55e4d6330389efe2a25ccb41d1d9dae95ad4341d
Rana Lulla <ranalulla@Ranas-MacBook-Pro.local>
2023-03-12 22:20:19 -0700

    integration testing for US9

---
+++ app/src/test/java/edu/ucsd/cse110/socialcompass/Story5IntegrationTest.java
@@ -0,0 +1,70 @@
+package edu.ucsd.cse110.socialcompass;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import edu.ucsd.cse110.socialcompass.model.Location;
+import edu.ucsd.cse110.socialcompass.model.LocationAPI;
+
+@RunWith(RobolectricTestRunner.class)
+public class Story5IntegrationTest {
+    @Test
+    public void getLocationTest() throws ExecutionException, InterruptedException, TimeoutException {
+        LocationAPI api = LocationAPI.provide();
+
+        Location loc = api.getLocation("ranatest");
+
+        assert loc.label.equals("Rana");
+    }
+
+    @Test
+    public void putLocationRunsTest() {
+        Location loc = new Location("ranatest2", 30, 40);
+        loc.private_code = "notouch";
+        loc.label = "Rana";
+
+        System.out.println(loc.toLimitedJSON(new String[]{"private_code", "label", "latitude", "longitude"}));
+
+        LocationAPI api = LocationAPI.provide();
+
+        api.putLocation(loc);
+    }
+
+    @Test
+    public void patchLocationRunsTest() {
+        Location loc = new Location("ranatest2");
+        loc.private_code = "notouch";
+        loc.isPublic = false;
+
+        System.out.println(loc.toLimitedJSON(new String[]{"private_code", "isPublic"}));
+
+        LocationAPI api = LocationAPI.provide();
+
+        api.patchLocation(loc);
+    }
+
+    @Test
+    public void getPutLocationTest() throws ExecutionException, InterruptedException, TimeoutException {
+        LocationAPI api = LocationAPI.provide();
+
+        Location loc = new Location("ranatest3", 35, 45);
+        loc.private_code = "notouch";
+        loc.label = "Rana";
+
+        api.putLocation(loc);
+
+        Thread.currentThread().join(200);
+
+        Location receivedLoc = api.getLocation("ranatest3");
+
+        assert receivedLoc.longitude == loc.longitude
+                && receivedLoc.latitude == loc.latitude
+                && receivedLoc.label.equals(loc.label);
+
+        api.deleteLoc(loc);
+    }
+}
--- app/src/test/java/edu/ucsd/cse110/socialcompass/Story9IntegrationTest.java
+++ app/src/test/java/edu/ucsd/cse110/socialcompass/Story9IntegrationTest.java
@@ -1,70 +1,85 @@
 package edu.ucsd.cse110.socialcompass;
 
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.MutableLiveData;
+import androidx.room.SkipQueryVerification;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.filters.FlakyTest;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
-
-import edu.ucsd.cse110.socialcompass.model.Location;
-import edu.ucsd.cse110.socialcompass.model.LocationAPI;
-
 @RunWith(RobolectricTestRunner.class)
 public class Story9IntegrationTest {
+    @Rule
+    public GrantPermissionRule finePermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+    @Rule
+    public GrantPermissionRule coarsePermissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
     @Test
-    public void getLocationTest() throws ExecutionException, InterruptedException, TimeoutException {
-        LocationAPI api = LocationAPI.provide();
+    public void test1UID() {
+        var scenarioAddFriend = ActivityScenario.launch(AddFriendActivity.class);
+        scenarioAddFriend.moveToState(Lifecycle.State.CREATED);
+        scenarioAddFriend.moveToState(Lifecycle.State.STARTED);
 
-        Location loc = api.getLocation("ranatest");
+        scenarioAddFriend.onActivity(activity -> {
+            TextView uidEntry = (TextView) activity.findViewById(R.id.enterFriendID);
+            Button submit_button = (Button) activity.findViewById(R.id.addFriendSubmitButton);
+            uidEntry.setText("ranatest2");
+            submit_button.performClick();
+        });
 
-        assert loc.label.equals("Rana");
+        var scenario = ActivityScenario.launch(CompassActivity.class);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            boolean uidExists=false;
+            for(int i=0; i<activity.uids.length; i++) {
+                if(activity.uids[i].equals("ranatest2")) uidExists=true;
+            }
+            assert uidExists;
+        });
     }
 
     @Test
-    public void putLocationRunsTest() {
-        Location loc = new Location("ranatest2", 30, 40);
-        loc.private_code = "notouch";
-        loc.label = "Rana";
+    public void test2UID() {
+        var scenarioAddFriend = ActivityScenario.launch(AddFriendActivity.class);
+        scenarioAddFriend.moveToState(Lifecycle.State.CREATED);
+        scenarioAddFriend.moveToState(Lifecycle.State.STARTED);
 
-        System.out.println(loc.toLimitedJSON(new String[]{"private_code", "label", "latitude", "longitude"}));
+        scenarioAddFriend.onActivity(activity -> {
+            TextView uidEntry = (TextView) activity.findViewById(R.id.enterFriendID);
+            Button submit_button = (Button) activity.findViewById(R.id.addFriendSubmitButton);
+            uidEntry.setText("ranatest2");
+            submit_button.performClick();
+            uidEntry.setText("ranatest5");
+            submit_button.performClick();
+        });
 
-        LocationAPI api = LocationAPI.provide();
+        var scenario = ActivityScenario.launch(CompassActivity.class);
+        scenario.moveToState(Lifecycle.State.STARTED);
 
-        api.putLocation(loc);
-    }
-
-    @Test
-    public void patchLocationRunsTest() {
-        Location loc = new Location("ranatest2");
-        loc.private_code = "notouch";
-        loc.isPublic = false;
-
-        System.out.println(loc.toLimitedJSON(new String[]{"private_code", "isPublic"}));
-
-        LocationAPI api = LocationAPI.provide();
-
-        api.patchLocation(loc);
-    }
-
-    @Test
-    public void getPutLocationTest() throws ExecutionException, InterruptedException, TimeoutException {
-        LocationAPI api = LocationAPI.provide();
-
-        Location loc = new Location("ranatest3", 35, 45);
-        loc.private_code = "notouch";
-        loc.label = "Rana";
-
-        api.putLocation(loc);
-
-        Thread.currentThread().join(200);
-
-        Location receivedLoc = api.getLocation("ranatest3");
-
-        assert receivedLoc.longitude == loc.longitude
-                && receivedLoc.latitude == loc.latitude
-                && receivedLoc.label.equals(loc.label);
-
-        api.deleteLoc(loc);
+        scenario.onActivity(activity -> {
+            boolean uid1Exists=false;
+            for(int i=0; i<activity.uids.length; i++) {
+                if(activity.uids[i].equals("ranatest2")) uid1Exists=true;
+            }
+            boolean uid2Exists=false;
+            for(int i=0; i<activity.uids.length; i++) {
+                if(activity.uids[i].equals("ranatest5")) uid2Exists=true;
+            }
+            assert uid1Exists && uid2Exists;
+        });
     }
 }
